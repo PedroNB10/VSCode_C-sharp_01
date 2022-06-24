7770b75b881cf9d11bdb3028a3bff5964963c232
Pedro Nogueira Barboza <pedro.nogueira.barboza@gmail.com>
2022-06-24 08:57:58 -0300

    criação da primeira classe

---
+++ Primeiro_Projeto/ContaCorrente.cs
@@ -0,0 +1,9 @@
+public class ContaCorrente 
+{
+
+   public string Titular;
+   public int numero_conta;
+   public int Agencia;
+   public double Saldo;
+
+}
---
+++ Primeiro_Projeto/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PRIMEIRO_PROJETO
+
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //tipo conta corrente, conta é a variavel
+          ContaCorrente conta = new ContaCorrente();
+            
+        }
+
+    }
+
+}
+
+
+
+
+// See https://aka.ms/new-console-template for more information
