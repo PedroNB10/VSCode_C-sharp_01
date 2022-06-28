3047ffe7e44ee6fb21eac795bd237a8dcddc2493
Pedro Nogueira Barboza <pedro.nogueira.barboza@gmail.com>
2022-06-28 17:06:27 -0300

    aula 02 c#

--- Primeiro_Projeto/ContaCorrente.cs
+++ Primeiro_Projeto/ContaCorrente.cs
@@ -1,9 +1,23 @@
 public class ContaCorrente 
 {
 
+
+//Títular, numero_conta, Agencia e Saldo são ATRIBUTOS DE CLASSE
    public string Titular;
    public int numero_conta;
    public int Agencia;
    public double Saldo;
+
+   public ContaCorrente (string Titular,  int Agencia, int numero_conta, double Saldo)
+    {
+
+      this.Titular = Titular;
+      this.numero_conta = numero_conta;
+      this.Agencia = Agencia;
+      this.Saldo = Saldo;
+      
+ Console.WriteLine("A conta é do(a) "+ this.Titular + ". A agência é "+ this.Agencia + ". O número da conta é "+this.numero_conta + ". O saldo é de: "+this.Saldo+" reais.");
+      
+   }
 
 }
--- Primeiro_Projeto/Program.cs
+++ Primeiro_Projeto/Program.cs
@@ -8,8 +8,35 @@ namespace PRIMEIRO_PROJETO
     {
         static void Main(string[] args)
         {
-            //tipo conta corrente, conta é a variavel
-          ContaCorrente conta = new ContaCorrente();
+            //tipo de variavel "ContaCorrente", "conta" é a variavel
+             ContaCorrente conta_do_pedro = new ContaCorrente("Pedro Nogueira",111,223,1000); // conta_do_pedro é uma variável que tem um objeto
+             ContaCorrente conta_do_Ednaldo = new ContaCorrente("Ednaldo",222,233,3000);
+             ContaCorrente conta_da_Cecilia = new ContaCorrente("Cecilia",333,226,-1000);
+
+            // conta_do_pedro.Titular = "Pedro Nogueira";
+            // conta_do_pedro.Agencia = 111;
+            // conta_do_pedro.numero_conta = 223;
+            // conta_do_pedro.Saldo = 1000;
+        
+
+            // conta_do_Ednaldo.Titular = "Ednaldo";
+            // conta_do_Ednaldo.Agencia = 111;
+            // conta_do_Ednaldo.numero_conta = 245;
+            // conta_do_Ednaldo.Saldo = -1000;
+            
+
+            // conta_da_Cecilia.Titular = "Cecilia";
+            // conta_da_Cecilia.Agencia = 111;
+            // conta_da_Cecilia.numero_conta = 233;
+            // conta_da_Cecilia.Saldo = 200;
+         
+
+             //A conta é do(a) Pedro Nogueira. A agência é 111. O número da conta é 223. O saldo é de: 1000 reais.
+            // Console.WriteLine("A conta é do(a) "+ conta_do_pedro.Titular + ". A agência é "+ conta_do_pedro.Agencia + ". O número da conta é "+conta_do_pedro.numero_conta + ". O saldo é de: "+conta_do_pedro.Saldo+" reais.");
+            // Console.WriteLine("A conta é do(a) "+ conta_do_Ednaldo.Titular + ". A agência é "+ conta_do_Ednaldo.Agencia + ". O número da conta é "+conta_do_Ednaldo.numero_conta + ". O saldo é de: "+conta_do_Ednaldo.Saldo+" reais.");
+            // Console.WriteLine("A conta é do(a) "+ conta_da_Cecilia.Titular + ". A agência é "+ conta_da_Cecilia.Agencia + ". O número da conta é "+conta_da_Cecilia.numero_conta + ". O saldo é de: "+conta_da_Cecilia.Saldo+" reais.");
+            
+    
             
         }
 
